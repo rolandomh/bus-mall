6a1e7a8a365cb7471f83e6397360a0970996b43a
RolandoMH <ro@spacelabforever.com>
2020-08-20 20:07:15 -0700

    bus-mall graph

--- Js/app.js
+++ Js/app.js
@@ -1,99 +1,171 @@
-//GOAL: render two cat pictures to the page
-// get the 'cat-container' parent element
-var parentElement = document.getElementById('lab-assets');
-
-
-// I'm gonna need a constructor function
-// filepath
-// alt
-// title
-var imageArray = [];
-
-function image(name, ext){
-  this.filepath = `../img/${name}.${ext}`;
-  this.alt = name;
-  this.title = name;
-  this.clicks = 0;
-
-  imageArray.push(this);
+var parentElement = document.getElementById('here');
+var parentList = document.getElementById('list')
+var itemArray = [];
+var uniqueImageArray = [];
+var maxClick = 25;
+var totalArray = [];
+function Item(filepath, alt, title) {
+  this.filepath = filepath;
+  this.alt = alt;
+  this.title = title;
+  this.clickCount = 0;
+  this.itemShown = 0;
+  itemArray.push(this);
 }
-new image('bag','jpg');
-new image('bathroom','jpg');
-new image('boots','jpg');
-new image('breakfast','jpg');
-new image('pen','jpg');
-new image('bubblegum','jpg');
-new image('chair','jpg');
-new image('cthulhu','jpg');
-new image('dog-duck','jpg');
-new image('dragon','jpg');
-new image('dog-duck','jpg');
-new image('banana','jpg');
-new image('pet-sweep','jpg');
-new image('scissors','jpg');
-new image('shark','jpg');
-new image('sweep','png');
-new image('tauntaun','jpg');
-new image('unicorn','jpg');
-new image('water-can','jpg');
-new image('wine-glass','jpg');
-new image('usb','gif');
-
-// create a function that will get a random image
-// get a random number between 0 and the length of the catArray
-// assign that random number to index number in the catArray
-// that will be that image that we show
-
-function getRandomImage(){
-  // get a random number from the helper function betweet 0 and one less than the length of the array
-  var randomIndex = getRandomNumber(imageArray.length);
-
-  // use that random number as the index for our catArray
-  var chosenImage = imageArray[randomIndex];
-
-  // create an img tag
+function checkLocalStorage() {
+  if (localStorage.getItem('items') === null) {
+    createItems();
+  } else {
+    var getItemArray = localStorage.getItem('items');
+    var parseItem = JSON.parse(getItemArray);
+    itemArray = parseItem;
+  }
+}
+checkLocalStorage();
+function createItems() {
+   new Item('../img/bag.jpg', 'bag', 'bag');
+  var banana = new Item('../img/banana.jpg', 'banana', 'banana');
+  var bathroom = new Item('../img/bathroom.jpg', 'bathroom', 'bathroom');
+  var boots = new Item('../img/boots.jpg', 'boots', 'boots');
+  var breakfast = new Item('../img/breakfast.jpg', 'breakfast', 'breakfast');
+  var bubblegum = new Item('../img/bubblegum.jpg', 'bubblegum', 'bubblegum');
+  var chair = new Item('../img/chair.jpg', 'chair', 'chair');
+  var cthulhu = new Item('../img/cthulhu.jpg', 'cthulhu', 'cthulhu');
+  var dogDuck = new Item('../img/dog-duck.jpg', 'dog duck', 'dog duck');
+  var dragon = new Item('../img/dragon.jpg', 'dragon', 'dragon');
+  var pen = new Item('../img/pen.jpg', 'pen', 'pen');
+  var sweep = new Item('../img/pet-sweep.jpg', 'pet sweep', 'pet sweep');
+  var scissors = new Item('../img/scissors.jpg', 'scissors', 'scissors');
+  var shark = new Item('../img/shark.jpg', 'shark', 'shark');
+  var sweep = new Item('../img/sweep.png', 'sweep', 'sweep');
+  var tauntaun = new Item('../img/tauntaun.jpg', 'tauntaun', 'tauntaun');
+  var unicorn = new Item('../img/unicorn.jpg', 'unicorn', 'unicorn');
+  var usb = new Item('../img/usb.gif', 'usb', 'usb');
+  var waterCan = new Item('../img/water-can.jpg', 'water-can', 'water-can');
+  var wineGlass = new Item('../img/wine-glass.jpg', 'wine glass', 'wine glass')
+}
+function random() {
+  var randomImage = randomNumber(itemArray.length);
+  while (uniqueImageArray.includes(randomImage)) {
+    randomImage = randomNumber(itemArray.length);
+  }
+  uniqueImageArray.push(randomImage);
+  if (uniqueImageArray.length > 6) {
+    uniqueImageArray.shift();
+  }
+  var chooseImage = itemArray[randomImage];
+  chooseImage.itemShown++;
   var imageElement = document.createElement('img');
-  // give that img tag a src = the path of where my image is
-  imageElement.setAttribute('src', chosenImage.filepath);
-  // give the img tag an alt
-  imageElement.setAttribute('alt', chosenImage.alt);
-  // give the img tag a title
-  imageElement.setAttribute('title', chosenImage.title);
-  // append it to the parent
+  imageElement.setAttribute('src', chooseImage.filepath);
+  imageElement.setAttribute('alt', chooseImage.alt);
+  imageElement.setAttribute('title', chooseImage.title);
   parentElement.appendChild(imageElement);
 }
-
-// helper function - got this from mdn
-function getRandomNumber(max) {
+function randomNumber(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-
-function handleClick(event){
-
-  // HERE!!!    JSON.parce();     !!!HERE
-
-  // figure out what was clicked on
-  console.log(`event.target is ${event.target.alt}`);
+function onClick(event) {
   var alt = event.target.alt;
-
-  // loop through my catsArray until I find the alt that matches my alt
-  for(var i=0; i<imageArray.length; i++){
-    if(event.target.alt === imageArray[i].alt){
-      imageArray[i].clicks++;
+  maxClick--;
+  if (maxClick !== 0) {
+    for (var i = 0; i < itemArray.length; i++) {
+      if (alt === itemArray[i].alt) {
+        itemArray[i].clickCount++;
+      }
     }
+    parentElement.innerHTML = '';
+    random();
+    random();
+    random();
   }
-  // once I've found my object instance
-  // increment the clicks on that object instance
-  console.log('an image was clicked');
-  parentElement.innerHTML = '';
-  getRandomImage();
-  getRandomImage();
+  else {
+    var jsonItemArray = JSON.stringify(itemArray);
+    localStorage.setItem('items', jsonItemArray);
+    parentElement.innerHTML = '';
+    for (var i = 0; i < itemArray.length; i++) {
+      totalArray.push(itemArray[i].clickCount);
+      var li = document.createElement('li');
+      li.textContent = itemArray[i].alt + ' had ' + itemArray[i].clickCount + ' vote(s) and was shown ' + itemArray[i].itemShown + ' time(s).'
+      parentList.appendChild(li);
+    }
+    chart();
+  }
 }
-
-
-parentElement.addEventListener('click', handleClick);
-
-// initally generates the images on page load
-getRandomImage();
-getRandomImage();
-
+parentElement.addEventListener('click', onClick);
+random();
+random();
+random();
+// function buildRadioButton(){
+// var radio = document.createElement('input');
+// radio.setAttribute('type', 'radio');
+// radio.setAttribute('value', chooseImage.alt);
+// parentElement.appendChild(radio);
+// }
+function chart() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can'],
+      datasets: [{
+        label: '# of Votes',
+        data: totalArray,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
